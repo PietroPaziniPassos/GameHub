648ae13364e36d569a469f3b9aba9e27660936f0
PietroPazini <ppppazini@gmail.com>
2023-08-31 17:24:22 -0400

    GameHub improving input

--- GameHub.py
+++ GameHub.py
@@ -12,17 +12,24 @@ def choose_game():
     welcome()
     print("(1) Hangman (2) Guess the number (3) Guess the word")
 
-    game_number = int(input("Choose a game: "))
+    while(True):
+        game_number = int(input("Choose a game: "))
 
-    if(game_number == 1):
-        print("Loading Hangman...")
-        Hangman.play()
-    elif(game_number == 2):
-        print("Loading Guess the number...")
-        GuessTheNumber.play()
-    elif(game_number == 3):
-        print("Loading Guess the word")
-        GuessTheWord.play()
+        if(game_number == 1):
+            print("Loading Hangman...")
+            Hangman.play()
+            break
+        elif(game_number == 2):
+            print("Loading Guess the number...")
+            GuessTheNumber.play()
+            break
+        elif(game_number == 3):
+            print("Loading Guess the word")
+            GuessTheWord.play()
+            break
+        else:
+            print("You have to choose a number between 1 and 3!")
+            continue
 
 if(__name__ == "__main__"):
     choose_game()
