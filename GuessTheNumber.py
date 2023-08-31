dd1a2e0de995fa666ae91d009e39b5e1cc5ecfeb
PietroPazini <ppppazini@gmail.com>
2023-08-31 14:25:06 -0400

    GTN choose game difficulty function finished

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -13,3 +13,17 @@ def play():
 def load_secret_number():
     secret_number = random.randrange(1,101)
     return secret_number
+
+def choose_game_difficulty():
+    print("*Choose the game difficulty*")
+    print("(1) Easy (2) Medium (3) Hard")
+
+    difficulty = int(input("Game difficulty: "))
+    
+    if(difficulty == 1):
+        total_guesses = 20
+    elif(difficulty == 2):
+        total_guesses = 10
+    else:
+        total_guesses = 5
+    return total_guesses
