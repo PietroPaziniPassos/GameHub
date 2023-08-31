8cf8d31c222046ce1f560d05539fdcafc4cb34bc
PietroPazini <ppppazini@gmail.com>
2023-08-31 14:02:47 -0400

    GameHub choose game function finished

--- GameHub.py
+++ GameHub.py
@@ -8,3 +8,17 @@ def welcome():
     print("*************************")
     print()
 
+def choose_game():
+    print("(1) Hangman (2) Guess the number (3) Guess the word")
+
+    game_number = int(input("Choose a game: "))
+
+    if(game_number == 1):
+        print("Loading Hangman...")
+        Hangman.play()
+    elif(game_number == 2):
+        print("Loading Guess the number...")
+        GuessTheNumber.play()
+    elif(game_number == 3):
+        print("Loading Guess the word")
+        GuessTheWord.play()
