eafa301950f71e97bab54cab94902a8a90f38955
PietroPazini <ppppazini@gmail.com>
2023-08-31 15:18:00 -0400

    GTW load secret word function finished

--- GuessTheWord.py
+++ GuessTheWord.py
@@ -2,6 +2,7 @@ import random
 
 def play():
     welcome()
+    secret_word = load_secret_word()
     
 def welcome():
     print("*******************************")
@@ -9,5 +10,23 @@ def welcome():
     print("*******************************")
     print()
 
-
-    
+def load_secret_word():
+    fruits_file = open("Words\Fruits.txt", "r")
+    animals_file = open("Words/Animals.txt", "r")
+    names_file = open("Words/names.txt", "r")
+    words_list = []
+    for line in fruits_file:
+        line = line.strip()
+        words_list.append(line)
+    for line in names_file:
+        line = line.strip()
+        words_list.append(line)
+    for line in animals_file:
+        line = line.strip()
+        words_list.append(line)
+    fruits_file.close()
+    names_file.close()
+    animals_file.close()
+    randomNumber = random.randrange(0, len(words_list))
+    secret_word = words_list[randomNumber].upper()
+    return secret_word
