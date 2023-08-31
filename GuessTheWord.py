afc3fde3f5ddf438c765d45f9feb85d33f039e91
PietroPazini <ppppazini@gmail.com>
2023-08-31 15:04:44 -0400

    GTW welcome function finished

--- GuessTheWord.py
+++ GuessTheWord.py
@@ -1,1 +1,13 @@
 import random
+
+def play():
+    welcome()
+    
+def welcome():
+    print("*******************************")
+    print("Welcome to Guess the word game!")
+    print("*******************************")
+    print()
+
+
+    
