e94f091c703a7f71c0c4bd1419b6ab5ae09de4b9
PietroPazini <ppppazini@gmail.com>
2023-08-31 11:30:56 -0400

    Hangman loadSecretWord lists created

--- Hangman.py
+++ Hangman.py
@@ -13,4 +13,7 @@ def load_secret_word():
     fruits_file = open("Words/Fruits.txt", "r")
     names_file = open("Words/Names.txt", "r")
     animals_file = open("Words/Animals.txt", "r")
-
+    fruits_list = []
+    names_list = []
+    animals_list = []
+    all_words_list = []
