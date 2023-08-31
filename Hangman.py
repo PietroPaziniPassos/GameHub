32c42767f8aaf34ae35628dc67c58e9256d562bc
PietroPazini <ppppazini@gmail.com>
2023-08-31 13:45:36 -0400

    Hangman print looser message function finished

--- Hangman.py
+++ Hangman.py
@@ -136,4 +136,21 @@ def print_winner_message(secret_word):
     print("The word was ", secret_word)
 
 def print_looser_message(secret_word):
-    pass
+    print("You lost!")
+    print("The word was ", secret_word)
+    print("    _______________        ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\   ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/    ")
+    print(" |   XXX       XXX   |     ")
+    print(" |                   |     ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |       ")
+    print("   | I I I I I I I |       ")
+    print("   |  I I I I I I  |       ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
