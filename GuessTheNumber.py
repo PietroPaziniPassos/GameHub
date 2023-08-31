71f85530d383f7ec11e03a501867a29bc5cd9070
PietroPazini <ppppazini@gmail.com>
2023-08-31 14:17:54 -0400

    GTN play function

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -4,3 +4,10 @@ def welcome():
     print("*********************************")
     print("Welcome to Guess the number game!")
     print("*********************************")
+
+def play():
+    welcome()
+    secret_number = load_secret_number()
+    total_guesses = choose_game_difficulty()
+
+    
