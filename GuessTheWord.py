47787888eb002d5d1244b34d89318e6477bdfafd
PietroPazini <ppppazini@gmail.com>
2023-08-31 15:31:55 -0400

    GTW load secret word changes

--- GuessTheWord.py
+++ GuessTheWord.py
@@ -3,6 +3,9 @@ import random
 def play():
     welcome()
     secret_word = load_secret_word()
+    right_letters = ["_" for letter in secret_word]
+    for i in range(0, 5):
+        print(right_letters)
     
 def welcome():
     print("*******************************")
@@ -11,22 +14,12 @@ def welcome():
     print()
 
 def load_secret_word():
-    fruits_file = open("Words\Fruits.txt", "r")
-    animals_file = open("Words/Animals.txt", "r")
-    names_file = open("Words/names.txt", "r")
+    words_file = open("Words/Five_letters_words.txt", "r")
     words_list = []
-    for line in fruits_file:
+    for line in words_file:
         line = line.strip()
         words_list.append(line)
-    for line in names_file:
-        line = line.strip()
-        words_list.append(line)
-    for line in animals_file:
-        line = line.strip()
-        words_list.append(line)
-    fruits_file.close()
-    names_file.close()
-    animals_file.close()
+    words_file.close()
     randomNumber = random.randrange(0, len(words_list))
     secret_word = words_list[randomNumber].upper()
     return secret_word
