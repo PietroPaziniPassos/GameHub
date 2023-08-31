87280337cfce089b4992505c9c94806d375eece7
PietroPazini <ppppazini@gmail.com>
2023-08-31 14:19:14 -0400

    GTN load secret number function finished

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -10,4 +10,6 @@ def play():
     secret_number = load_secret_number()
     total_guesses = choose_game_difficulty()
 
-    
+def load_secret_number():
+    secret_number = random.randrange(1,101)
+    return secret_number
