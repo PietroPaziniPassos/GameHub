02d0d3db60af0894acec7a4df5fbaf888fa6920c
PietroPazini <ppppazini@gmail.com>
2023-08-31 11:30:10 -0400

    Hangman loadSecretWord files opened

--- Hangman.py
+++ Hangman.py
@@ -8,3 +8,9 @@ def welcome():
     print("***Welcome to hangman game!***")
     print("******************************")
     print()
+
+def load_secret_word():
+    fruits_file = open("Words/Fruits.txt", "r")
+    names_file = open("Words/Names.txt", "r")
+    animals_file = open("Words/Animals.txt", "r")
+
