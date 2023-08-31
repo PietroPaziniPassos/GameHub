465bc2d714930f483bf5d12922d42983582abf56
PietroPazini <ppppazini@gmail.com>
2023-08-31 17:21:49 -0400

    GTN improving imput

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -11,16 +11,18 @@ def play():
 
         guess = input("Input a number between 1 and 100: ")
         guess = int(guess)
-
-        if(guess == secret_number):
-            print_winner_message(secret_number)
-            break
+        if(guess > 0 and guess < 101):
+            if(guess == secret_number):
+                print_winner_message(secret_number)
+                break
+            else:
+                if(guess > secret_number):
+                    print("It's not it! Try a smaller number.")
+                elif(guess < secret_number):
+                    print("It's not it! Try a bigger number.")
+            attempt += 1
         else:
-            if(guess > secret_number):
-                print("It's not it! Try a smaller number.")
-            elif(guess < secret_number):
-                print("It's not it! Try a bigger number.")
-        attempt += 1
+            print("You can only guess a number between 1 and 100")
     if(attempt > total_attempts):
         print_looser_message(secret_number)
 
@@ -37,15 +39,21 @@ def load_secret_number():
 def choose_game_difficulty():
     print("*Choose the game difficulty*")
     print("(1) Easy (2) Medium (3) Hard")
+    while(True):
+        difficulty = int(input("Game difficulty: "))
 
-    difficulty = int(input("Game difficulty: "))
-
-    if(difficulty == 1):
-        total_guesses = 20
-    elif(difficulty == 2):
-        total_guesses = 10
-    else:
-        total_guesses = 5
+        if(difficulty == 1):
+            total_guesses = 20
+            break
+        elif(difficulty == 2):
+            total_guesses = 10
+            break
+        elif(difficulty == 3):
+            total_guesses = 5
+            break
+        else:
+            print("You can only choose 1, 2 or 3!")
+            continue
     return total_guesses
 
 def print_winner_message(secret_number):
