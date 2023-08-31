b0a9e36bd7b39c3bd346f46e2ce910e7e3bd7efd
PietroPazini <ppppazini@gmail.com>
2023-08-31 14:38:06 -0400

    GTN main game logic finished

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,14 +1,32 @@
 import random
 
+def play():
+    welcome()
+    secret_number = load_secret_number()
+    total_attempts = choose_game_difficulty()
+    attempt = 0
+
+    while(attempt <= total_attempts):
+        print("Attempt {} of {}".format(attempt, total_attempts))
+
+        guess = input("Input a number between 1 and 100: ")
+        guess = int(guess)
+
+        if(guess == secret_number):
+            print_winner_message(secret_number)
+            break
+        else:
+            if(guess > secret_number):
+                print("It's not it! Try a smaller number.")
+            elif(guess < secret_number):
+                print("It's not it! Try a bigger number.")
+    if(attempt > total_attempts):
+        print_looser_message(secret_number)
+
 def welcome():
     print("*********************************")
     print("Welcome to Guess the number game!")
     print("*********************************")
-
-def play():
-    welcome()
-    secret_number = load_secret_number()
-    total_guesses = choose_game_difficulty()
 
 def load_secret_number():
     secret_number = random.randrange(1,101)
@@ -19,7 +37,7 @@ def choose_game_difficulty():
     print("(1) Easy (2) Medium (3) Hard")
 
     difficulty = int(input("Game difficulty: "))
-    
+
     if(difficulty == 1):
         total_guesses = 20
     elif(difficulty == 2):
@@ -27,3 +45,9 @@ def choose_game_difficulty():
     else:
         total_guesses = 5
     return total_guesses
+
+def print_winner_message(secret_number):
+    pass
+
+def print_looser_message(secret_number):
+    pass
